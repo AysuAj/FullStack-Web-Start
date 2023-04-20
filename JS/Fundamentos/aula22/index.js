c39f370272ece4d714581954b1189d7274bc2cfa
Alessander Júnio <alessanderaj44@gmail.com>
2023-04-20 19:55:53 -0300

    commit exercicio 22 completo js

--- JS/Fundamentos/aula22/index.js
+++ JS/Fundamentos/aula22/index.js
@@ -1,2 +1,34 @@
 const turista = prompt("Informe o seu nome: ")
 
+let visita = prompt("Já visitou alguma cidade? (s/n)")
+
+let cidades
+
+let cont = 0
+
+if (visita == 's') {
+  let cidade = prompt("Qual o nome da cidade visitada? ")
+  cidades = cidade + ','
+  cont += 1
+  while (visita == 's'){
+    visita = prompt("Já visitou alguma outra cidade? (s/n)")
+    if (visita == 's') {
+      let cidade = prompt("Qual o nome da cidade visitada? ")
+      cidades += cidade + ','
+      cont += 1
+    } else {
+      visita == 'n'
+    }
+  }
+
+  if (cont == 1) {
+    alert(`${turista} você visitou ${cont} cidade`)
+    alert(`Cidade visitada: ${cidades}`)
+  } else {
+    alert(`${turista} você visitou ${cont} cidades`)
+    alert(`Cidades visitadas: ${cidades}`)
+  }
+  
+} else {
+  alert("Não visitou nenhuma cidade")
+}
