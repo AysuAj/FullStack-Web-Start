d30d31a7704e7a8200d81d84539b3fe1f12c0ebf
Alessander Júnio <alessanderaj44@gmail.com>
2023-04-26 21:36:26 -0300

    ajustes exercicio 24 js

--- JS/Fundamentos/aula24/exercicio1/index.js
+++ JS/Fundamentos/aula24/exercicio1/index.js
@@ -1,13 +1,12 @@
 const numero = parseInt(prompt("Informe o número: "))
-let string
+let string = 0
 let mult
 let strNumero
+let cont = 1
 
 for(let i = 1; i <= 20; i++) {
   mult = numero*i
-  strNumero = mult
-  strNumero = toString(strNumero) 
-  string = strNumero + ' - '
+  string += mult + '\n'
 }
 
 alert(string)
