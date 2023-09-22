a4eb5364d67a0cd21f8089e4b9259a5be8e862c4
Alessander Júnio <alessanderaj44@gmail.com>
2023-09-22 17:35:23 -0300

    exercicio 09 js

--- JS/Fundamentos/exercicio9/script.js
+++ JS/Fundamentos/exercicio9/script.js
@@ -14,49 +14,66 @@ while (validar == true) {
     Escolha:
     `)
 
-    switch (choose) {
+    if (choose == '1') {
+        areaTriangulo()
 
-        case '1':
-            areaTriangulo()
-            break
+    } else if (choose == '2') {
+        areaRetangulo()
+
+    } else if (choose == '3') {
+        areaQuadrado()
+
+    } else if (choose == '4') {
+        areaTrapezio()
+
+    } else if (choose == '5') {
+        areaQuadrado()
+
+    } else if (choose == '6') {
+        alert('Encerrando o programa...')
+        validar = false
     }
 }
 
+alert('Programa encerrado!')
+
 
 function areaTriangulo() {
-    let baseTriangulo = prompt('Base Triângulo: ')
-    let alturaTriangulo = prompt('Informe a altura do triângulo: ')
+    let baseTriangulo = parseFloat(prompt('Base Triângulo: '))
+    let alturaTriangulo = parseFloat(prompt('Informe a altura do triângulo: '))
 
-    return alert(`A área do triângulo com a medida da base ${baseTriangulo} e altura ${alturaTriangulo} é: ${(baseTriangulo * altura) / 2}`)
+    return alert(`A área do triângulo com medida da base: ${baseTriangulo} e altura: ${alturaTriangulo} é: ${(baseTriangulo * alturaTriangulo) / 2}`)
 
 }
 
 function areaRetangulo() {
-    let baseRetangulo = prompt('Base retângulo: ')
-    let alturaRetangulo = prompt('Altura retângulo: ')
+    let baseRetangulo = parseFloat(prompt('Base retângulo: '))
+    let alturaRetangulo = parseFloat(prompt('Altura retângulo: '))
 
-    return baseRetangulo * alturaRetangulo
+    return alert(`A área do retângulo com medida base: ${baseRetangulo} e altura: ${alturaRetangulo} é: ${baseRetangulo * alturaRetangulo}`)
 
 }
 
 function areaQuadrado() {
-    let ladoQuadrado = prompt('Medida lado quadrado: ')
+    let ladoQuadrado = parseFloat(prompt('Medida lado quadrado: '))
 
-    return Math.sqrt(ladoQuadrado)
+    return alert(`A área do quadrado com medida do lado: ${ladoQuadrado} é: ${Math.pow(ladoQuadrado, 2)}`)
 
 }
 
 function areaTrapezio() {
-    let baseMaiorTrapezio = prompt('Base Trapézio maior: ')
-    let baseMenorTrapezio = prompt('Base Trapézio menor: ')
-    let alturaTrapézio = prompt('Altura Trapézio: ')
+    let baseMaiorTrapezio = parseFloat(prompt('Base Trapézio maior: '))
+    let baseMenorTrapezio = parseFloat(prompt('Base Trapézio menor: '))
+    let alturaTrapézio = parseFloat(prompt('Altura Trapézio: '))
 
-    return ((baseMaiorTrapezio + baseMenorTrapezio) * alturaTrapézio) / 2
+    return alert(`A área do Trapézio com medida da base maior: ${baseMaiorTrapezio}, base menor: ${baseMenorTrapezio} e altura: ${alturaTrapézio} é igual: ${((baseMaiorTrapezio + baseMenorTrapezio) * alturaTrapézio) / 2}`)
 
 }
 
 function areaCirculo() {
-    let raioCirculo = prompt('Raio do círculo: ')
+    let raioCirculo = parseFloat(prompt('Raio do círculo: '))
 
-    return Math.PI * Math.sqrt(raioCirculo)
+    return alert(`A área do círculo com medida do raio: ${raioCirculo} é igual: ${Math.PI * Math.pow(raioCirculo, 2)}`)
 }
+
+marcelo128ufmg @gmail.com
